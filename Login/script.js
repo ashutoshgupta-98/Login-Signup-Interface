7eb1f9ef9b0721848e4c6159d2d1c0f49d5e35dc
unknown <ag23gupta@gmail.com>
2024-01-08 11:17:54 +0530

    files addes

---
+++ Login/script.js
@@ -0,0 +1,9 @@
+function showSignup() {
+  document.getElementById('login-form').style.display = 'none';
+  document.getElementById('signup-form').style.display = 'block';
+}
+
+function showLogin() {
+  document.getElementById('login-form').style.display = 'block';
+  document.getElementById('signup-form').style.display = 'none';
+}
